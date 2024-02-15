47a48b9318f565e9c6c1c850dfb7bc1849f070a5
hungpham2924 <160097521+hungpham2924@users.noreply.github.com>
2024-02-15 12:08:05 +0700

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = [" Nah I'd live"];
 
 
 let currentIndex = 0;
